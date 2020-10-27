2fb77f78a9002af877e1c5b42e8747b6bfefb603
Danil Sisov <56063160+danils2000@users.noreply.github.com>
2020-10-27 13:20:47 +0200

    Add files via upload

---
+++ School/ex1.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+float mean(int* array, int n);
+
+float mean(int* array, int n) {
+
+		float sum = 0;
+
+		for (int i = 0; i < n; i++) {
+			sum += array[i];
+		}
+		sum = sum / n;
+
+		return sum;
+	}
+
+
+int main() {
+	int input;
+    int num = 1;
+	int* numbers;
+
+	cout << "Enter how many numbers shoudl randomly generated: ";
+	cin >> input;
+
+	numbers = new int[input];
+
+if (input >= 0) { 
+    for (int i = 0; i < input; i++) {
+		numbers[i] = rand() % 100;
+        cout << num;
+        cout << ". ";
+		cout << numbers[i] << endl;
+        num++;
+	}
+    
+	cout << "Avarage:  " << mean(numbers, input) << endl;
+} 
+    else {
+        cout << "Erorr" << endl;
+        return 0;
+}
+
+	delete[] numbers;
+}
